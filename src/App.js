9e149a2fe993a3a1b975a83698da5b21b1126945
Fatima Basharat <fatima8746@gmail.com>
2023-07-29 15:09:17 -0400

    got data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import CitySearch from './CitySearch';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AirQualityCard from './AirQualityCard';
 
 function App() {
+  const [airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+      const data = await response.json()
+      console.log(data)
+      if(response.ok && data.status === 'ok'){
+        setAirQualityData(data.data)
+        setError(null)
+      } else {
+        setError("Sorry, we couldn't find the city you were looking for. Try another location nearby or ensure your spelling is correct.")
+        setAirQualityData(null)
+      }
+    } catch (error) {
+      console.error("network error:",error)
+      setError("Sorry, something went wrong.")
+      setAirQualityData(null)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <h1 className='mt-5 mb-3'>Air Quality Index Checker</h1>
+    <CitySearch getAirQuality={getAirQuality}/>
+    {error && (
+      <div className='alert alert-danger' role='alert'>
+        {error}
+      </div>
+    )}
+    {airQualityData && (
+      <>
+      <AirQualityCard data={airQualityData}/>
+
+
+      </>
+    )}
     </div>
   );
 }
