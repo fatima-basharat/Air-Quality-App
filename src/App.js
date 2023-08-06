473954ef416b14e0d569523c926af24fe145f3cf
Fatima Basharat <fatima8746@gmail.com>
2023-08-06 13:10:49 -0400

    completed project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AirQualityCard from './AirQualityCard';
 import PollutantInfo from './PollutantInfo';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import AirQualityLevelsTable from './AirQualityLevelsTable';
 
 function App() {
   const [airQualityData, setAirQualityData] = useState(null)
@@ -43,6 +44,8 @@ function App() {
       <PollutantInfo pollutant={airQualityData.dominentpol}/>
       </>
     )}
+
+    <AirQualityLevelsTable />
     </div>
   );
 }
