1c2a2d075a123236b06a32cc1609d1768425082b
Fatima Basharat <fatima8746@gmail.com>
2023-08-06 13:34:37 -0400

    fixed issues

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const getAirQuality = async (city) => {
     try {
-      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=08d2c26295fdaaa76780a94e2aae21a535eabe89`)
       const data = await response.json()
       console.log(data)
       if(response.ok && data.status === 'ok'){
